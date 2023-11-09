d1a074d0764c9593e93c246d7577b28910206156
Nahir Jara <nahiir066@gmail.com>
2023-11-09 11:34:59 -0300

    Segunda subida

--- servidorExpress.js
+++ servidorExpress.js
@@ -1,13 +1,35 @@
 const express = require('express')
-const app = express()
+const server = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   res.send('Hello World!')
 })
-  
 
-app.listen(port, () => {
+server.get('/buscarUsuario', (req, res) => {
+  const encontrado = true
+
+  if (encontrado) {
+    res.status(200).send('Status 200: El usuario fué encontrado');
+  } else {
+    res.status(404).send('Status 404: No se encontró el usuario');
+  }
+})
+
+server.get('/200',(req, res) => {
+
+   res.status(200).send('Status 200: OK');
+})
+
+server.get('/301',(req, res)=> {
+  res.status(301).send('Status 301: La página se trasladó a otro lugar');
+})
+
+server.get('/400',(req, res)=> {
+  res.status(400).send('Status 400: Bad request. Ocurrió un error');
+})
+
+server.listen(port, () => {
   console.log(`Servidor corriendo en el puerto: ${port}`)
 })
 
